2daf6ec858862c59836fdba4100d1e4bbc34383b
Christian <christianmacarthur17@gmail.com>
2023-06-26 22:05:43 +0200

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(pdf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/files",
+          outputPath: "static/files",
+          name: "[name].[hash].[ext]",
+          esModule: true,
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
